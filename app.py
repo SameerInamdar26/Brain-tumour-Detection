ed0a56014cc834ce5644010e2c4ab522f3d56690
Sameer <saminamdar2310@gmail.com>
2025-10-30 09:00:22 +0530

    Adding Styling and Js And app.js Changes

--- app.py
+++ app.py
@@ -57,5 +57,8 @@ def predict():
                            confidence=round(confidence*100, 2),
                            cam_image=f"uploads/cam_{filename}")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
